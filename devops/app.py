a3f2465f67686edd57639b0f67b1bf527c688216
Wellington Roque <wellington.roque@gmail.com>
2026-03-05 22:10:41 -0300

    diciona rota de livros

--- devops/app.py
+++ devops/app.py
@@ -14,5 +14,9 @@ def sobre():
 def livros():
     return "Lista de livros cadastrados"
 
+@app.route("/cadastro-livro")
+def cadastro_livro():
+    return "Página de cadastro de livros"
+
 if __name__ == "__main__":
     app.run(debug=True)
