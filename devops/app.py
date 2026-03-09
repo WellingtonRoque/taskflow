2dbcd5010e6a36ba202abbfa496b10cd69e18e78
Wellington Roque <wellington.roque@fatec.corp>
2026-03-09 10:17:01 -0300

    adiciona rota cadastro

--- devops/app.py
+++ devops/app.py
@@ -1,20 +1,18 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Sistema de Gerenciamento de Biblioteca"
+    return "Sistema de Gestão de Biblioteca"
 
 @app.route("/sobre")
 def sobre():
-    return "Projeto desenvolvido na disciplina de Integração e Entrega Contínua"
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
 
 @app.route("/livros")
 def livros():
     return "Lista de livros cadastrados"
-
 
 @app.route("/cadastro-livro")
 def cadastro_livro():
