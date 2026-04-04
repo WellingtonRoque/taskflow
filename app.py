d23a1cba35f3e75a8693533dcfe7dd137f33b2e9
Wellington Roque <wellington.roque@gmail.com>
2026-04-04 11:28:19 -0300

    feat: estrutura inicial com CI

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema de Gestão de Biblioteca"
+
+@app.route("/sobre")
+def sobre():
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
+
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
+
+@app.route("/cadastro-livro")
+def cadastro_livro():
+    return "Página de cadastro de livros"
+
+if __name__ == "__main__":
+    app.run(debug=True)
