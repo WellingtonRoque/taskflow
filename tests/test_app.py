d98b7112d273e0e9ca863001557feab6f9b6c805
Wellington Roque <wellington.roque@gmail.com>
2026-04-26 10:35:31 -0300

    fix: corrige path para testes no CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
 from app import app
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 def test_status():
     client = app.test_client()
